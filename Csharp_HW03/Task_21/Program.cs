bf627eab7f94558a9a97bb86fd5ba1484caefcea
Elvis <elvisnepresly@yandex.ru>
2023-01-10 00:37:52 +0300

    Task 21

---
+++ Csharp_HW03/Task_21/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("X1");
+int x1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("y1");
+int y1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("z1");
+int z1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("X2");
+int x2= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("y2");
+int y2= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("z2");
+int z2= Convert.ToInt32(Console.ReadLine());
+double rasst = Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)),2);
+Console.WriteLine($"Расстояние в пространсве равно {rasst}"); 
